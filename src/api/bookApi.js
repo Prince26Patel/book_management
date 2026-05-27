afe470bb1af9ae967bc547e5aeb735d7de1e595a
princeBrumainc <Prince.patel@brumainc.com>
2026-05-27 12:26:51 +0530

    added backend url

--- src/api/bookApi.js
+++ src/api/bookApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/books";
+const API_URL = "bookmanagement-production-9712.up.railway.app";
 
 export const getBooks = async () => {
   const response = await axios.get(API_URL);
